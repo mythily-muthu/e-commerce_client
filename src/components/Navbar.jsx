259779cbfe5c7d04d5a847c592ae0488e7ee8899
mythily-muthu <mythilimuthu18@gmail.com>
2023-03-24 15:41:07 +0530

    Added navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,76 @@
 import React from "react";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import { Link } from "react-router-dom";
+import "./Navbar.scss";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        {/* left side */}
+        <div className="left">
+          <div className="item">
+            <img
+              src="https://cdn.firstcry.com/education/2022/04/24105849/193394741.jpg"
+              alt=""
+              width="30px"
+              height="17px"
+            />
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className="item">
+            <span>USD</span>
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className="item">
+            <Link to="/products/1">Men</Link>
+          </div>
+          <div className="item">
+            <Link to="/products/2">Women</Link>
+          </div>
+          <div className="item">
+            <Link to="/products/3">Children</Link>
+          </div>
+          <div className="item">
+            <Link to="/products/4">Accessories</Link>
+          </div>
+        </div>
+        {/* center */}
+        <div className="center">
+          <Link to="/">THREADS & TRENDS</Link>
+        </div>
+
+        {/* right side */}
+        <div className="right">
+          <div className="item">
+            <Link to="/">Homepage</Link>
+          </div>
+          <div className="item">
+            <Link to="/">About</Link>
+          </div>
+          <div className="item">
+            <Link to="/">Contact</Link>
+          </div>
+          <div className="item">
+            <Link to="/">Stores</Link>
+          </div>
+          <div className="icons">
+            <SearchIcon />
+            <PersonOutlineOutlinedIcon />
+            <FavoriteBorderOutlinedIcon />
+            <div className="cartIcon">
+              <ShoppingCartOutlinedIcon />
+              <span>0</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
