d123a57268e5647aee39c1201f42e8501bd7e418
mythily-muthu <mythilimuthu18@gmail.com>
2023-03-24 13:35:10 +0530

    Added needed pages and components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer/Footer";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Products from "./Pages/Products/Products";
+import Product from "./Pages/Product/Product";
 
-function App() {
+
+const Layout = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>hi</h1>
-      </header>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+}
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/products/:id",
+        element: <Products />,
+      },
+      {
+        path: "/product/:id",
+        element: <Product />,
+      },
+    ]
+  }
+]);
+function App() {
+  return (
+    <div>
+      <RouterProvider router={router} />
     </div>
   );
 }
