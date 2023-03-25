f6a428ca3f1038069fd356e594d3779e6157865a
mythily-muthu <mythilimuthu18@gmail.com>
2023-03-25 10:52:07 +0530

    Added slider, footer, and card component

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import FeaturedProducts from "../../components/FeaturedProducts/FeaturedProducts";
+import Slider from "../../components/Slider/Slider";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Slider />
+      <FeaturedProducts type="featured" />
+      <FeaturedProducts type="trending" />
+    </div>
+  );
 };
 
 export default Home;
---
+++ src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import Card from "../Card/Card";
+
+import "./FeaturedProducts.scss";
+const FeaturedProducts = ({ type }) => {
+  const data = [
+    {
+      id: 1,
+      img: "https://images.pexels.com/photos/1972115/pexels-photo-1972115.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      img2: "https://images.pexels.com/photos/1163194/pexels-photo-1163194.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      title: "Long Sleeve Graphic T-shirt",
+      isNew: true,
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 2,
+      img: "https://images.pexels.com/photos/1759622/pexels-photo-1759622.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      title: "coat",
+      isNew: true,
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 3,
+      img: "https://images.pexels.com/photos/1457983/pexels-photo-1457983.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      title: "Skirt",
+      isNew: true,
+      oldPrice: 19,
+      price: 12,
+    },
+    {
+      id: 4,
+      img: "https://images.pexels.com/photos/2065200/pexels-photo-2065200.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      title: "Hat",
+      isNew: true,
+      oldPrice: 19,
+      price: 12,
+    },
+  ];
+  return (
+    <div className="FeaturedProducts">
+      <div className="top">
+        <h1>{type} Products</h1>
+        <p>
+          Find out how to look stylish every day with these style resources.
+          From how-to, style guides, stylish essentials, what to wear guides and
+          so much more! These blog posts will help you figure out how to put a
+          stylish outfit together and master the art of color coordinating and
+          pattern mixing. Read every guide or use the outfits for your
+          inspiration.
+        </p>
+      </div>
+      <div className="bottom">
+        {data.map((item) => {
+          <Card item={item} key={item.id} />;
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default FeaturedProducts;
